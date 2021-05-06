f536e8e495b1c3d733a5946e02e110f2f9b1e984
harshaathnere <harshaathnere@gmail.com>
2021-05-06 19:52:16 +0530

    updation

--- src/component/Video/index.js
+++ src/component/Video/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import {Col,} from 'react-bootstrap';
 import SingleVideo from './SingleVideo';
 import SuggestionVideo from './SuggestionVideo';
+import YouTube from 'simple-youtube-api';
+import config from '../../config';
 
+const youtube = new YouTube('config.apiKey');
 export default () => {
-    const callApi = () => {
-        
+
+    useEffect(()=>{
+        callApi();
+    },[]);
+ const result1 =   youtube.searchVideos('Centuries', 4)
+    .then(results => {
+        console.log(`The video's title is ${results[0].title}`);
+    })
+    console.log(result1)
+    .catch(console.log);
+    const callApi = async () => {
+   const result = await youtube.search("ramtoo",5)
+   console.log(result);
     }
     return (
         <React.Fragment>
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    apiKey:"AIzaSyDbrdqviThSuBhNCSkZjvOTgu1WlihoWII"
+}
